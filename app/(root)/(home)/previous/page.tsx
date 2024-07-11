77cb7c71dced37f7881ccfa29ddbf28b74c21692
Pratham2712 <prathamvaishya123@gmail.com>
2024-07-11 23:06:04 +0530

    initial

---
+++ app/(root)/(home)/previous/page.tsx
@@ -0,0 +1,8 @@
+export default function Previous() {
+    return (
+        <section className="flex size-full flex-col gap-10 text-white">
+            <h1 className="text-3xl font-bold"> previous</h1>
+
+        </section>
+    )
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import Mobile from "./MobileNav";
+
+export default function Navbar() {
+    return (
+        <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+            <Link href='/' className="flex items-center gap-1">
+                <Image src="./icons/logo.svg" width={32} height={32} alt="YOOM" className="max-sm:size-10" />
+                <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+                    YOOM
+                </p>
+            </Link>
+            <div className="flex-between gap-5">
+                <Mobile />
+            </div>
+        </nav>
+    )
+}
